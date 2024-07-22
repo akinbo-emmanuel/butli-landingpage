fb59de8edf4527149c2cf3de05587420a5aa6eef
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2024-07-22 08:05:28 +0100

    Minor updates

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -28,6 +28,7 @@ const Courses = () => {
       control.start("hidden");
     }
   }, [control, inView]);
+
   return (
     <section
       id="courses"
@@ -45,9 +46,10 @@ const Courses = () => {
           >
             <CardHeader>
               <CardTitle>Basic Leadership Course</CardTitle>
-              <CardDescription className="text-[#6b7280] text-xs">
+              <CardDescription className="text-[#6b7280]">
                 A foundational course designed to introduce essential leadership
-                principles for adults.
+                principles.
+                <br /> <br /> <span className="font-semibold text-[#428c9d]">Anticipate!!! Coming up in September</span>
               </CardDescription>
             </CardHeader>
             <CardFooter>
@@ -60,17 +62,19 @@ const Courses = () => {
             variants={boxVariant}
             initial="hidden"
             animate={control}
-            className="box bg-white text-[#428c9d] border rounded-lg"
+            className="box bg-white text-[#428c9d] border rounded-lg flex flex-col justify-between"
           >
             <CardHeader>
               <CardTitle>Advanced Leadership Course</CardTitle>
               <CardDescription className="text-[#6b7280]">
-                An in-depth course for experienced leaders
-                looking to enhance their skills.
+                An in-depth course for experienced leaders looking to enhance
+                their skills.
               </CardDescription>
             </CardHeader>
             <CardFooter>
-              <Button>Enroll Now</Button>
+              <Button disabled className="disabled:cursor-not-allowed">
+                Enroll Now
+              </Button>
             </CardFooter>
           </motion.div>
 
@@ -79,17 +83,19 @@ const Courses = () => {
             variants={boxVariant}
             initial="hidden"
             animate={control}
-            className="box bg-white text-[#428c9d] border rounded-lg md:col-span-2 lg:col-span-1"
+            className="box bg-white text-[#428c9d] border rounded-lg md:col-span-2 lg:col-span-1 flex flex-col justify-between"
           >
             <CardHeader>
               <CardTitle>Executive Leadership Course</CardTitle>
               <CardDescription className="text-[#6b7280]">
-                A comprehensive course for executives to
-                refine their leadership strategies.
+                A comprehensive course for executives to refine their leadership
+                strategies.
               </CardDescription>
             </CardHeader>
             <CardFooter>
-              <Button>Enroll Now</Button>
+              <Button disabled className="disabled:cursor-not-allowed">
+                Enroll Now
+              </Button>
             </CardFooter>
           </motion.div>
         </div>
